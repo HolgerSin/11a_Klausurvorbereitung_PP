0024e1b13a11ada4ee508c1e987229acb0f71d5c
Holger <86824184+HolgerSin@users.noreply.github.com>
2021-09-02 12:36:12 +0200

    myChanges

--- funktionen.js
+++ funktionen.js
@@ -1,2 +1,33 @@
+"use strict";
+
 /* DATENSTRUKTUREN */
+
+// let arr = ["a", "b", "c", "d"];
+
+// console.log(arr[-1]);
+
+
+// let myObj = {
+//         x:10, 
+//         y:"Hallo", 
+//         sub1:{
+//             val1: "hi",
+//             val2: "world"
+//         }
+
+// };
+
+// console.log(myObj);
+// console.log(myObj.x);
+// console.log(myObj.y);
+
+
+// console.log(myObj.sub1);
+
+
+console.log(document.body.children[0].innerHTML);
+document.body.children[0].innerHTML="TestText"
+document.getElementById("test").innerHTML="Text";
+
+
 
